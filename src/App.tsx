449b83b1a5630534c6153f5626fd84960d637f67
Vijay Sharma <vijay.sharma@appinventiv.com>
2020-04-24 19:35:25 +0530

    fixed imports

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import { StyleSheet } from 'react-native';
 
 //custom imports below
 import store from './store';
-import Home from './screens/home';
 import Route from './router';
 
 export default function App() {
   return (
     <Provider store={store}>
-      {/* <Home /> */}
       <Route />
     </Provider>
   );
